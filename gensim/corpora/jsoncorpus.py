0414eca706cead969b2487b7ac9bdc5f4b7c80c6
Stephan Gabler <stephan.gabler@gmail.com>
2011-07-18 17:39:33 +0200

    add more flexible preprocessing to the jsoncorpus

--- gensim/corpora/jsoncorpus.py
+++ gensim/corpora/jsoncorpus.py
@@ -16,23 +16,21 @@ import sys
 import traceback
 
 logger = logging.getLogger('gensim.corpora.jsoncorpus')
-#dictionary.filterExtremes(noBelow=2) # post-process the dictionary: remove tokens that are too frequent or too infrequent
-#14:46:35 Jose Quesada: before (in the mar2008 english parsing) we used <10
-#14:46:45 Stephan Gabler: ok
-#14:46:55 Jose Quesada: for too frequent, we can use a stoplist
-#14:47:05 Jose Quesada: there should be many for german floating around
-#14:47:25 Stephan Gabler: ok
-#14:47:46 Jose Quesada: but you can tell gensim to drop anything above fq=10000 or somesuch
+
 
 class JsonCorpus(corpora.TextCorpus):
 
-    def __init__(self, input, dictionary=None, no_below=10, no_above=0.5):
+    def __init__(self, input,
+                 dictionary=None,
+                 no_below=10, no_above=0.5,
+                 preprocess=[]):
         """
         Initialize the corpus. This scans the corpus once, to determine its
         vocabulary (only the first `keep_words` most frequent words that
         appear in at least `noBelow` documents are kept).
         """
         self.input = input
+        self.preprocess = preprocess
         if dictionary:
             self.dictionary = dictionary
         else:
@@ -53,24 +51,17 @@ class JsonCorpus(corpora.TextCorpus):
         """
         length = 0
         for line in self.getstream():
-            if length < 999000:
-                continue
-            try:
-                article = json.loads(line)
-            except Exception as inst:
-                print line
-                print inst
-                for tb in traceback.format_tb(sys.exc_info()[2]):
-                    print tb
+            article = json.loads(line)
 
             s = u""
             for chapter in article[article.keys()[0]].itervalues():
                 s = s + u" " + chapter[0]
             length += 1
-            if (length % 1000) == 0:
-                print length
 
-            yield utils.tokenize(s, lowercase=True)
+            s = utils.tokenize(s, lowercase=True)
+            for f in self.preprocess:
+                s = f(s)
+            yield s
         self.length = length
 
     def __len__(self):
